1bf5b41df01945bcaf8f00cc44f041d0ec5724dd
Aram Harutyunyan <you@example.com>
2020-05-30 15:42:00 +0400

    Have added the changes to the Master branch as well

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import {getFromBackEnd} from "./utils/http.js";
 console.log(getFromBackEnd())
 
 const shopItemsContainer = document.getElementById("items");
-
+let cartNumber=[];
 getFromBackEnd()
  .then(res=>{
      console.log("res", res);
@@ -15,11 +15,25 @@ getFromBackEnd()
                     <h2>${item.price}</h2>
                     <img src="${item.img}">
                     <h1>${item.title}</h1>
-                    <input type="button" value="Add to cart"
+                    <input type="button" value="Add to cart" onclick="addToCart(${item.id})" id="${item.id}">
+                    
                     </div>
-                </div>`
+        </div>`
+
+                
      })
+
      shopItemsContainer.innerHTML=items;
      console.log("items", items)
  })
+
+ window.addToCart=function (itemID){
+    cartNumber.push(itemID)
+    document.getElementById("cart").innerHTML=cartNumber.length;
+    return disableButton(itemID);
+ }
+function disableButton(itemID){
+    let button=document.getElementById(itemID);
+    button.disabled=true;
+}
 console.log("shopItemsContainer", shopItemsContainer)
