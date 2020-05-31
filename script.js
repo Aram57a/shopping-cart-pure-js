d16e0d64c9a65370b3699001852ec13a02477f5d
Aram Harutyunyan <you@example.com>
2020-05-31 21:54:29 +0400

    added till the change of server info to json file

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import {getFromBackEnd} from "./utils/http.js";
 console.log(getFromBackEnd())
 
 const shopItemsContainer = document.getElementById("items");
+const cartItems = document.getElementById("modal-items");
 let cartNumber=[];
 getFromBackEnd()
  .then(res=>{
@@ -15,25 +16,23 @@ getFromBackEnd()
                     <h2>${item.price}</h2>
                     <img src="${item.img}">
                     <h1>${item.title}</h1>
-                    <input type="button" value="Add to cart" onclick="addToCart(${item.id})" id="${item.id}">
-                    
+                    <input type="button" value="Add to cart" onclick="addToCart(${item.id}, ${item})" id="${item.id}"> 
                     </div>
-        </div>`
-
-                
+        </div>`           
      })
-
      shopItemsContainer.innerHTML=items;
      console.log("items", items)
  })
 
- window.addToCart=function (itemID){
+ window.addToCart=function (itemID,item){
     cartNumber.push(itemID)
-    document.getElementById("cart").innerHTML=cartNumber.length;
+    document.getElementById("cart-number").innerHTML=cartNumber.length;
+    cartItems.innerHTML=item;
     return disableButton(itemID);
  }
 function disableButton(itemID){
     let button=document.getElementById(itemID);
     button.disabled=true;
 }
+document.getElementById("icon")
 console.log("shopItemsContainer", shopItemsContainer)
