1fce745f135df321dbdacb9883de77ee3169ab6e
Aram Harutyunyan <you@example.com>
2020-06-06 11:36:18 +0400

    Final version with bags :)

--- script.js
+++ script.js
@@ -1,40 +1,98 @@
-import {getFromBackEnd} from "./utils/http.js";
-console.log(getFromBackEnd())
-
+let res =[
+    {
+      "id": 0,
+      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
+      "title": "Samsung Galaxy",
+      "price": 550
+    },
+    {
+      "id": 1,
+      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
+      "title": "Samsung Galaxy",
+      "price": 550
+    },
+    {
+      "id": 2,
+      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
+      "title": "Samsung Galaxy",
+      "price": 550
+    },
+    {
+      "id": 3,
+      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
+      "title": "Samsung Galaxy",
+      "price": 550
+    },
+    {
+      "id": 4,
+      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
+      "title": "Samsung Galaxy",
+      "price": 550
+    },
+    {
+      "id": 5,
+      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
+      "title": "Samsung Galaxy",
+      "price": 550
+    }
+  ] 
 const shopItemsContainer = document.getElementById("items");
-//const cartItems = document.getElementById("modal-items");
-let cartNumber=[];
-/*getFromBackEnd()
- .then(res=>{
-     console.log("res", res);
-     
-     let items="";
-     res.forEach(item=>{
-         items += `
-         <div class="grid-item">
-                    <div class="flex-cartandphoto">
-                    <h2>${item.price}</h2>
-                    <img src="${item.img}">
-                    <h1>${item.title}</h1>
-                    <input type="button" value="Add to cart" onclick="addToCart(${item.id}, ${item})" id="${item.id}"> 
-                    </div>
-        </div>`           
-     })
-     shopItemsContainer.innerHTML=items;
-     console.log("items", items)
- })
-*/
+let cartItems = document.getElementById("modal-items");
+let cartNumber=0;
+const getFromBackEnd=()=>{
+    let items="";
+    res.forEach(item=>{
+        items += `
+        <div class="grid-item">
+                   <div class="flex-cartandphoto">
+                   <h2>${item.price}</h2>
+                   <img src="${item.img}">
+                   <h1>${item.title}</h1>
+                   <button onclick="addToCart(${item.id})" id="${item.id}"> Add to Cart </button>
+                   </div>
+       </div>`           
+    })
+    return items;
+}
 shopItemsContainer.innerHTML=getFromBackEnd();
-console.log("items", items)
-window.addToCart=function (itemID,item){
-    cartNumber.push(itemID)
-    document.getElementById("cart-number").innerHTML=cartNumber.length;
-  //  cartItems.innerHTML=item;
+window.addToCart=function (itemID){
+    cartNumber++;
+    document.getElementById("cart-number").innerHTML= cartNumber;
+    let itemPrice=res.find(el=>el.id==itemID).price;
+    let itemTitle=res.find(el=>el.id==itemID).title;
+    cartItems.innerHTML+=`<div class="modal-items-flex ">
+                           <div>
+                            <h2>Item Price: ${itemPrice}</h2>
+                            <h2 class="underscore">Item Name: ${itemTitle}</h2>
+                           </div>
+                           <div class="modal-item-delete">+</div>
+                           </div>
+                           `
+    console.log(cartItems)
     return disableButton(itemID);
  }
 function disableButton(itemID){
     let button=document.getElementById(itemID);
     button.disabled=true;
 }
-//document.getElementById("icon")
-console.log("shopItemsContainer", shopItemsContainer);
+let cartButton= document.getElementById("icon-basket")
+cartButton.onclick=function (){
+  document.querySelector(".bg-modal").style.display="flex";
+
+}
+let closeButton=document.getElementById("close-icon")
+closeButton.onclick=function (){
+  document.querySelector(".bg-modal").style.display="none";
+}
+/*
+let removeButton=document.querySelector(".modal-item-delete");
+
+removeButton.onclick=function(e){
+  e.target.parentElement.remove();
+}*/
+cartItems.addEventListener('click', (e) => {
+  document.getElementById("cart-number").innerHTML--;
+  const btn = e.target.closest('.modal-item-delete');
+  //if (!btn) return;
+  btn.parentElement.remove();
+});
--- utils/http.js
+++ utils/http.js
@@ -1,55 +1,9 @@
-export const getFromBackEnd=()=>{
-
-  let res =[
-    {
-      "id": 0,
-      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
-      "title": "Samsung Galaxy",
-      "price": 550
-    },
-    {
-      "id": 1,
-      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
-      "title": "Samsung Galaxy",
-      "price": 550
-    },
-    {
-      "id": 2,
-      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
-      "title": "Samsung Galaxy",
-      "price": 550
-    },
-    {
-      "id": 3,
-      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
-      "title": "Samsung Galaxy",
-      "price": 550
-    },
-    {
-      "id": 4,
-      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
-      "title": "Samsung Galaxy",
-      "price": 550
-    },
-    {
-      "id": 5,
-      "img": "https://www.sunrise.ch/en/residential/geraete/handys/apple-iphone-11-pro-max.primaryproductimage.code-MDAwMDAwMDAwMDAwMDIwMTc4.format-hardware-configurator-l.png",
-      "title": "Samsung Galaxy",
-      "price": 550
+function time12to24(str){
+    if (str[5]=="P" || str[6]=="P"){
+    console.log(Number(str.substr(0,5).split(":")[0])+12+":"+str.substr(0,5).split(":")[1]);
     }
-  ] 
-     let items="";
-     res.forEach(item=>{
-         items += `
-         <div class="grid-item">
-                    <div class="flex-cartandphoto">
-                    <h2>${item.price}</h2>
-                    <img src="${item.img}">
-                    <h1>${item.title}</h1>
-                    <input type="button" value="Add to cart" onclick="addToCart(${item.id}, ${item})" id="${item.id}"> 
-                    </div>
-        </div>`           
-     })
-     return items;
-
- }
+    else {
+        console.log(str.substr(0,5));
+    }
+}
+time12to24("08:45 AM")
